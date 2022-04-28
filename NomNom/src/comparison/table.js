023dd1955d6fad5238b5c60b0de946d152b27300
Laney <laneyluong@gmail.com>
2022-04-27 18:31:34 -0700

    add table and table items

--- NomNom/src/comparison/table.js
+++ NomNom/src/comparison/table.js
@@ -1,11 +1,25 @@
 import TableItem from "./table_item";
+import './table.css'
 
-function Table() {
+function Table({dogFood, nomFood}) {
+
   return (
     <div className="table">
-      COMPARISON CALCULATOR
-      Table
-      <TableItem />
+      {dogFood.name} contains {dogFood.minProtein}% min protein, {dogFood.minFat}% min fat and has a calorie density of { dogFood.calorie}kcal/kg. <br/>
+
+
+      <div className='table-row'> 
+
+        <div className='table-item dog-food'>
+          <TableItem food={dogFood} isNom={false}/>
+        </div>
+
+        <div className='table-item'>
+          <TableItem food={nomFood} isNom={true}/>
+        </div>
+
+      </div>
+
     </div>
   );
 }
--- NomNom/src/comparison/table_item.js
+++ NomNom/src/comparison/table_item.js
@@ -1,9 +1,17 @@
-function TableItem() {
+function TableItem({food, isNom}) {
+  const textAlign = isNom ? 'left' : 'right';
+
   return (
     <div className="table-item">
-      Table
+      <div className='table-item-food-image'>Insert Image Here</div>
+      <div style={{'text-align': textAlign}}>
+        <p>Protein</p><h3>{food.protein}</h3>
+        <p>Fat</p>  <h3>{food.fat}</h3>
+        <p>Carbohydrate </p> <h3>{food.carbs}</h3>
+      </div>
     </div>
   );
+
 }
 
 export default TableItem;
