576576ddf7a4d33c2bc88e64bb90cef014f1dfab
Laney <laneyluong@gmail.com>
2022-04-27 19:26:00 -0700

    add images and fix styling

--- NomNom/src/comparison/table_item.js
+++ NomNom/src/comparison/table_item.js
@@ -4,11 +4,13 @@ function TableItem({className, food, isNom}) {
   return (
     <div className={`table-item ${className}`}>
 
-      {isNom ? nomFoodName(food.name, alignment) : ''}
+      <div className='nom-food-header'>
+        {isNom ? nomFoodName(food.name, alignment) : ''}
+      </div>
 
+      <img src={require(`../images/${food.img_name}`)} className='table-item-food-image' style={{ 'float': alignment }} alt={food.name}/>
 
-      <div className='table-item-food-image' style={{ 'float': alignment }}>Insert Image Here</div>
-      <div style={{ 'text-align': alignment, 'clear': alignment}}>
+      <div style={{ textAlign: alignment, 'clear': alignment}}>
         <p>Protein</p><h3>{food.protein}</h3>
         <p>Fat</p>  <h3>{food.fat}</h3>
         <p>Carbohydrate </p> <h3>{food.carbs}</h3>
@@ -21,7 +23,7 @@ function TableItem({className, food, isNom}) {
 
 const nomFoodName = (name, alignment) => {
   return (
-    <div style={{ 'text-align': alignment }}>
+    <div style={{ textAlign: alignment }}>
       <h3>NOM NOM</h3>
       <h4>{name}</h4>
     </div>
